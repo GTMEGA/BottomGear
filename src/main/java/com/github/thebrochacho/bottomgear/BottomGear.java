b6d3fa85b5ccc223e6f48abade38349329c5688b
Matt <mtthw8198@gmail.com>
2021-11-05 11:30:21 -0700

    Added a debug item that spits out all NEI subset tags or all modded equipment

--- src/main/java/com/github/thebrochacho/bottomgear/BottomGear.java
+++ src/main/java/com/github/thebrochacho/bottomgear/BottomGear.java
@@ -2,13 +2,11 @@ package com.github.thebrochacho.bottomgear;
 
 import com.github.thebrochacho.bottomgear.events.MobSpawnListener;
 import com.github.thebrochacho.bottomgear.events.PlayerListener;
-import com.github.thebrochacho.bottomgear.item.DebugStick;
-import cpw.mods.fml.common.FMLCommonHandler;
+import com.github.thebrochacho.bottomgear.item.BottomStick;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
-import net.minecraft.block.material.Material;
 import net.minecraft.item.Item;
 import net.minecraftforge.common.MinecraftForge;
 
@@ -18,7 +16,7 @@ public class BottomGear {
     public static final String NAME = "Bottom Gear";
     public static final String VERSION = "@version@";
 
-    public static final DebugStick DEBUG_STICK = new DebugStick(Item.ToolMaterial.IRON);
+    public static final Item DEBUG_STICK = new BottomStick();
 
     @EventHandler
     public void init(FMLInitializationEvent event) {
